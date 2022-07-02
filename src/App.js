e6e722fd70fa92dc6d13e75ea09a4f3222f40ce6
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-07-02 00:19:58 -0700

    Crate Dashboard and Login singup Functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Layout from "./components/pages/Layout";
 import LoginSingup from "./components/pages/auth/LoginReg";
+import SendPasswordResetEmail from "./components/pages/auth/SendPasswordResetEmail";
+import ResetPassword from "./components/pages/auth/ResetPassword";
+import Dashboard from "./components/pages/Dashboard";
 
 function App() {
   return (
@@ -13,7 +16,11 @@ function App() {
             <Route index element={<Home />} />
             <Route path="contact" element={<Contact />} />
             <Route path="login" element={<LoginSingup />} />
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+            <Route path="reset" element={<ResetPassword />} />
           </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<h1>ASN 404 Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
